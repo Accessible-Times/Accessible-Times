f6e36fcf26f72345405b5e65ee4c0bc05451f6b4
Khushi1148 <101751936+Khushi1148@users.noreply.github.com>
2024-12-08 19:11:04 +0530

    Update text_to_image.py
    Updated the code.

--- backend/text_to_image.py
+++ backend/text_to_image.py
@@ -1,11 +1,8 @@
 import requests
 import time
 
-
 image_api_key = "5JKBQ0GMfj2ujVa8VzYObwcl2tgjrw"
 prompt = "The content discusses the launch of a flagship product from Google that competes with the iPhone 16 and Galaxy S24. It also highlights the latest advancements in AI technology, along with the challenges and opportunities it presents now and in the future. Additionally, it mentions how 3D printing is revolutionizing sneaker design, especially in smaller brands, and discusses Chegg's layoff of 319 workers due to competition with modern AI chatbots."
-
-
 
 def image_post_function(api_key, prompt):
     """
@@ -35,8 +32,6 @@ def image_post_function(api_key, prompt):
     
     else:
         return "Error in POST"
-
-
  
 def image_get_function(request_id, api_key):
     """
@@ -53,27 +48,6 @@ def image_get_function(request_id, api_key):
     return response_imageurl_containing_json.json()
 
 
-
-def extract_individual_url(response_imageurl_containing_json):
-    """
-    To extract the individual image urls from the json response
-    """
-    try:
-        all_urls_lst = []
-        obtained_url_lst =  response_imageurl_containing_json.get('images')
-        for item in obtained_url_lst:
-            image_url = item.get('url')
-            if image_url == None:
-                return "Please try again"
-            all_urls_lst.append(image_url)
-
-        return all_urls_lst
-
-    except Exception as e:
-        print(f"An error occurred : {e}")
-
-
-
 def generate_image_urls(image_api_key, prompt):
     """
     Main Function to be called for image generation
@@ -82,19 +56,22 @@ def generate_image_urls(image_api_key, prompt):
 
         response_json = image_post_function(image_api_key, prompt)
         request_id = response_json.get("id")
+        print(request_id)
 
         # Adding some wait time so that the Image URL can be released by the API
-        count = 0
-        while count<5 :
-            time.sleep(5)
-            count += 1
-        
-        response_imageurl_containing_json = image_get_function(request_id, image_api_key)
+        while True:
+            response_imageurl_containing_json = image_get_function(request_id, image_api_key)
+            images_urls = [image["url"] for image in response_imageurl_containing_json.get('images', [])]
+            if images_urls != [None]:
+                break
+            time.sleep(3)
     
-        url_lst = extract_individual_url(response_imageurl_containing_json)
-        return url_lst
+        return images_urls
 
     except Exception as e:
         print(e)
+
+
+
 
 
